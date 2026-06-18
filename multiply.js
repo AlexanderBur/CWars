c72a06e4baa3aba81f1720305c35b4af9f2d5ecf
AleksanderBu <moroz_bu@proton.me>
2026-06-18 22:24:36 +0500

    Multiply finish

---
+++ multiply.js
@@ -0,0 +1,8 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+    if (typeof(a) !== "number" || typeof(b) !== "number") {
+        return 0;
+    } else {
+        return a * b;
+    }}
