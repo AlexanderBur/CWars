e8c816d4bb9bb8d578c07f738567ceae87ae597b
AleksanderBu <moroz_bu@proton.me>
2026-06-21 18:21:07 +0500

    array + array 2 solution ready

---
+++ array-and-array.js
@@ -0,0 +1,21 @@
+function arrayPlusArray(arr1, arr2) {
+
+    const sum1 = arr1.reduce((summ,number) => summ + number, 0);
+    const sum2 = arr2.reduce((summ,number) => summ + number, 0);
+
+    return  sum1 + sum2;
+
+}
+
+function arrayPlusArray(arr1, arr2) {
+
+    let TotalSumm = 0;
+    for (let i = 0 ; i < arr1.length ; i++){
+        TotalSumm += arr1[i];
+    }
+    for (let i = 0 ; i < arr2.length ; i++){
+        TotalSumm += arr2[i];
+    }
+
+    return TotalSumm
+}
