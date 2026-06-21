a5de85094ed4bcf4f53bc77a8876be6eb20f2bc4
AleksanderBu <moroz_bu@proton.me>
2026-06-21 17:50:11 +0500

    smallest number ready with best practice

--- smallest-result.js
+++ smallest-result.js
@@ -7,14 +7,42 @@
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 //
 
-function findSmallestInt(arr) {
-    let smallestResult = 10000000000000000;
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < smallestResult) {
-            smallestResult = arr[i];
-        }
+// function findSmallestInt(arr) {
+//     let smallestResult = arr[0];
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < smallestResult) {
+//             smallestResult = arr[i];
+//         }
+//
+//     }
+//
+//     return smallestResult;
+// }
 
-    }
-
-    return smallestResult;
+function findSmallestIntMathMin(arr) {
+    return Math.min(...arr);
 }
+
+// Math.min(...arr) разворачивает весь массив в отдельные аргументы функции, поэтому на очень больших массивах
+// может возникнуть ошибка из-за превышения допустимого количества аргументов.
+// Обычный цикл for работает надёжнее и использует меньше дополнительной памяти.
+
+function findSmallestIntForLoop(arr) {
+    let min = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+const largeArray = Array.from({ length: 1000000 }, () => Math.floor(Math.random() * 2000000) - 1000000);
+
+console.time('Math.min');
+findSmallestIntMathMin(largeArray);
+console.timeEnd('Math.min');
+
+console.time('For loop');
+findSmallestIntForLoop(largeArray);
+console.timeEnd('For loop');
